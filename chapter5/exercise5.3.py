23faefefb28de69b69bccf5173fa2b82cc086767
halixzed <harrisahmad641@gmial.com>
2022-11-03 11:20:28 +0000

    Re-adding Exercise 5.3

--- chapter5/exercise5.3.py
+++ chapter5/exercise5.3.py
@@ -1,26 +1,37 @@
 '''Exercise 5.3'''
 
-def num_digits(x = 0):
+def num_digits(x):
      # remove the following line and write the function here
-  try:
-    x = int(input("Please enter a positive whole number:\n"))
-    counter = 0
-    a = x
-    if type(a) == int:
-      while  a > 1:
-        a = a / 10
-        counter += 1
-      print(f"{x} has {counter} digits.")
-      return None
-  except:
-    print ("Invalid input.")
+  counter = 0
+  q = x
+  if q < 0:
+    q = q * -1
+    
+  while  q >= 1:
+    q = q / 10
+    counter += 1
+  return counter
   
 
 def main():
  # remove the following line and write your program here
   # x = num_digits(x)
   # print(f"{x} has {num_digits}")
-  num_digits()
+  try:
+    x = int(input("Please enter a positive whole number:\n"))
+    if type(x) == int:
+      if x != 0:
+        if num_digits(x) > 1:
+          print (f"{x} has {num_digits(x)} digits.")
+        else:
+          print (f"{x} has {num_digits(x)} digit.")
+      if x == 0:
+        print("Invalid input.")
+          
+  except:
+    print ("Invalid input.")
+    
+    
   
 
 # This is needed for the tests. This is now the first bit of code Python will run.
